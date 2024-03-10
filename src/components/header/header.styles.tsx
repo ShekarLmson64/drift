b367cf8db1ed04e6f94dff27a12a13b53cf24d3e
Shekar Abraboiena <shekarlmson1997@gmail.com>
2024-03-10 18:13:00 +0530

    added pageLayout, favicon, css

--- src/components/header/header.styles.tsx
+++ src/components/header/header.styles.tsx
@@ -6,5 +6,5 @@ export const MainBox = styled(Box)(() => ({
     zIndex: "9",
     position: "fixed",
     top: 0,
-    padding: `${DesktopPxToVw(10)} ${DesktopPxToVw(60)}` // 10px to top-bottom and 60px to right-left
+    padding: `${DesktopPxToVw(20)} ${DesktopPxToVw(80)}` // 10px to top-bottom and 60px to right-left
 }))
---
+++ src/layout/pageLayout.tsx
@@ -0,0 +1,11 @@
+import Header from '@/components/header/header.component'
+import { Box } from '@mui/material'
+import React from 'react'
+
+export default function PageLayout() {
+  return (
+    <Box>
+        <Header/>
+    </Box>
+  )
+}
